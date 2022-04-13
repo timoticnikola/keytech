8edd0874f8a7e04882fb613d776b5e52c136afa5
Nikola Timotic <timoticnikola.01@gmail.com>
2022-04-13 20:42:26 +0200

    Added SlideShow

--- assets/js/main.js
+++ assets/js/main.js
@@ -655,7 +655,74 @@ window.onload = () => {
 		resetBtn.addEventListener("click", resetFilters);
 	} else if (url == "/index.html") {
 		topDiscountProducts();
+		let slideIndex = 0;
+		let slideTimer;
+		const slideDelay = 3;
+		const slides = document.getElementsByClassName("slide");
+		ShowSlide = (index) => {
+			if (index == slides.length) slideIndex = 0;
+			else if (index < 0) slideIndex = slides.length - 1;
+			else slideIndex = index;
+			for (i = 0; i < slides.length; i++) {
+				slides[i].style.display = "none";
+			}
+			SlideReset();
+			slides[slideIndex].style.display = "block";
+		};
+		SlideReset = () => {
+			window.clearInterval(slideTimer);
+			setTimeout(function () {
+				slideTimer = window.setInterval(function () {
+					ShowSlide((slideIndex += 1));
+				}, slideDelay * 1000);
+			}, 10);
+		};
+		neg.addEventListener("click", function () {
+			ShowSlide((slideIndex -= 1));
+		});
+		pos.addEventListener("click", function () {
+			ShowSlide((slideIndex += 1));
+		});
+		ShowSlide(slideIndex);
 	}
+
+	// Slideshow
+	// let slideIndex = 0;
+	// let slideTimer;
+	// const slideDelay = 3;
+	// const slides = document.getElementsByClassName("slide");
+
+	// ShowSlide = (index) => {
+	// 	if (index == slides.length) slideIndex = 0;
+	// 	else if (index < 0) slideIndex = slides.length - 1;
+	// 	else slideIndex = index;
+
+	// 	for (i = 0; i < slides.length; i++) {
+	// 		slides[i].style.display = "none";
+	// 	}
+
+	// 	SlideReset();
+	// 	slides[slideIndex].style.display = "block";
+	// };
+
+	// SlideReset = () => {
+	// 	window.clearInterval(slideTimer);
+	// 	setTimeout(function () {
+	// 		slideTimer = window.setInterval(function () {
+	// 			ShowSlide((slideIndex += 1));
+	// 		}, slideDelay * 1000);
+	// 	}, 10);
+	// };
+
+	// neg.addEventListener("click", function () {
+	// 	ShowSlide((slideIndex -= 1));
+	// });
+	// pos.addEventListener("click", function () {
+	// 	ShowSlide((slideIndex += 1));
+	// });
+
+	// ShowSlide(slideIndex);
+
 	function topDiscountProducts() {
 		let productsArray = jsonParse("productsLocal");
 		let filteredDiscount = productsArray.filter(function (el) {
--- test.js
+++ test.js
@@ -1,15 +1,35 @@
-console.log(1);
-function test() {
-  return new Promise((resolve) => {
-    console.log("Test function");
-    resolve();
-  });
+let slideIndex = 0;
+let slideTimer;
+const slideDelay = 3;
+const slides = document.getElementsByClassName("slide");
 
-}
+ShowSlide = (index) => {
+	if (index == slides.length) slideIndex = 0;
+	else if (index < 0) slideIndex = slides.length - 1;
+	else slideIndex = index;
 
-async function test5() {
-  console.log("Before");
-  await test();
-  console.log("After");
-}
-test5();
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+
+	SlideReset();
+	slides[slideIndex].style.display = "block";
+};
+
+SlideReset = () => {
+	window.clearInterval(slideTimer);
+	setTimeout(function () {
+		slideTimer = window.setInterval(function () {
+			ShowSlide((slideIndex += 1));
+		}, slideDelay * 1000);
+	}, 10);
+};
+
+neg.addEventListener("click", function () {
+	ShowSlide((slideIndex -= 1));
+});
+pos.addEventListener("click", function () {
+	ShowSlide((slideIndex += 1));
+});
+
+ShowSlide(slideIndex);
